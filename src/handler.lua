b0bf12ea152b5fa074a251f5838e0134e87b5170
vikaskmr1008 <vikaskmr1008@gmail.com>
2017-09-21 14:22:53 +0530

    Refactor code

--- src/handler.lua
+++ src/handler.lua
@@ -1,9 +1,6 @@
 -- load the base plugin object
 local BasePlugin = require "kong.plugins.base_plugin"
 local responses = require "kong.tools.responses"
---local requests = require('requests')
---local json = require "json"
-
 local constants = require "kong.constants"
 
 -- local header_filter = require "kong.plugins.token-validation.header_filter"
@@ -25,8 +22,6 @@ local get_method = ngx.req.get_method
 
 local set_uri_args = ngx.req.set_uri_args
 local get_uri_args = ngx.req.get_uri_args
-
-local ngx_log = ngx.log
 
 -- creating a subclass 
 local plugin = BasePlugin:extend()
@@ -50,15 +45,13 @@ function plugin:access(plugin_conf) -- Executed for every request upon it's rece
   local request_uri = ngx.var.uri
   
   if request_uri ~= login_uri then
-    ngx.log(ngx.ERR, "============ excuting if block ============")
       -- local authorization_header = request.get_headers()["x-authorization"]
       local authorization_header = req_get_headers()["x-authorization"]
-    
       ngx.log(ngx.ERR, "============ authorization_header ============" .. authorization_header)
-      
+        
         if not authorization_header then 
           -- throw error here
-        ngx.log(ngx.ERR, "============exiting if block bz authorization_header is null ============" .. authorization_header)
+          ngx.log(ngx.ERR, "============exiting if block bz authorization_header is null ============" .. authorization_header)
           return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
 
         else  
@@ -76,30 +69,14 @@ function plugin:access(plugin_conf) -- Executed for every request upon it's rece
               }
           
           })
-      --headers = {["Content-Type"] = "application/json"}
-      --response = requests.get{url = "http://iam_con.weave.local:9049/iam/v1/oauth/" .. authorization_header .. "/validate", headers = headers}
-      
-      
-      --local encode = json:encode(res)
-      --ngx.log(ngx.ERR, encode)
-      --local decode = json:decode(res)
-      --ngx.log(ngx.ERR, decode)
-      
-      --ngx.log(ngx.ERR, response)
-      
-      --ngx.log(ngx.ERR, res)
-      --ngx.log(ngx.ERR, res.body)
         
         local json = cjson.decode(res.body)
-        
-        --ngx.log(ngx.ERR, cjson.decode(res.body))
-        ngx.log(ngx.ERR, tostring(json))
       
         local statusCode = json.data.statusCode
         local isValid = json.data.valid
         
-      ngx.log(ngx.ERR, statusCode)
-      ngx.log(ngx.ERR, isValid)
+        ngx.log(ngx.ERR, statusCode)
+        ngx.log(ngx.ERR, isValid)
         
         if not statusCode and isValid  then
             ngx.status = 501
