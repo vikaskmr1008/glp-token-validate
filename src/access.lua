50a4b1d74e6ac1af9592822853a6b42b85374238
vikas.kumar3 <vikas.kumar3@DEL1-LHP-N02092.synapse.com>
2017-09-22 14:32:58 +0530

    Refactor code

--- src/access.lua
+++ src/access.lua
@@ -24,7 +24,7 @@ function _M.execute(plugin_conf)
             --ssl_verify = false,
             headers = {
                 ["Content-Type"] = "application/json",
-                --["CorrelationId"] = "12345"
+                ["CorrelationId"] = "12345"
               }
           })
         
